bc5e16ce428717c1cbf71749ea517a79d3dc652a
agilman <gilmanalex@gmail.com>
2022-11-03 23:49:55 -0700

    Created a django project. Started a django app. Added a model.

---
+++ treeMapperAPI/mappingAPI/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+class TreeSpecies(models.Model):
+    genus = models.CharField(max_length=32)
+    species = models.CharField(max_length=32)
+    commonName = models.CharField(max_length=64)
+    wiki = models.CharField(max_length=128)
+
+class Parks(models.Model):
+    name = models.CharField(max_length=32)
+
+class Trees(models.Model):
+    species = models.ForeignKey(TreeSpecies,on_delete=models.CASCADE)
+    park = models.ForeignKey(Parks,on_delete=models.DO_NOTHING)
+    lat = models.DecimalField(max_digits=9, decimal_places=6)
+    lng = models.DecimalField(max_digits=9, decimal_places=6)
+    size = models.IntegerField()   
+
+class TreePhotos(models.Model):
+    tree = models.ForeignKey(Trees,on_delete=models.DO_NOTHING)
+    ts = models.DateTimeField()
