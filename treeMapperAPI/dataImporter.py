927584dfa0efa4e09f1beac11f72e84cfc87df23
agilman <gilmanalex@gmail.com>
2022-11-04 11:22:32 -0700

    Created a routine for adding initial data (Parks, tree species)

---
+++ treeMapperAPI/dataImporter.py
@@ -0,0 +1,26 @@
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "treeMapperAPI.settings")
+import django
+django.setup()
+
+from mappingAPI import models
+
+
+myParks = ["Elizabeth"]
+print("Adding parks:")
+for i in myParks:
+    print(i)
+    newPark = models.Parks(name=i)
+    newPark.save()
+
+
+print("\nAdding Trees:")
+
+trees = [{'genus':'Acer','species':'rubrum','commonName':'Red Maple','wiki':'https://en.wikipedia.org/wiki/Acer_rubrum'},
+         {'genus':'Acer','species':'platanoides','commonName':'Norway Maple','wiki':'https://en.wikipedia.org/wiki/Acer_platanoides'},
+         {'genus':'Acer','species':'saccharum','commonName':'Sugar Maple','wiki':'https://en.wikipedia.org/wiki/Acer_saccharum'}
+]
+for i in trees:
+    print(i["commonName"])
+    newTree = models.TreeSpecies(genus=i['genus'], species=i['species'], commonName=i['commonName'], wiki=i['wiki'])
+    newTree.save()
