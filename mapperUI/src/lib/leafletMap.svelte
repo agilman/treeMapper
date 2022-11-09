f09379a6dee57cebc9134069c6a814af0b674d44
agilman <gilmanalex@gmail.com>
2022-11-08 23:53:20 -0800

    loading center

--- mapperUI/src/lib/leafletMap.svelte
+++ mapperUI/src/lib/leafletMap.svelte
@@ -1,15 +1,18 @@
 <script>
-    import { onMount, onDestroy } from 'svelte';
+    import { onMount, onDestroy, tick } from 'svelte';
     import.meta.env.SSR
 
+    export let mapCenter;
+    export let mapZoom;
+    
     let mapElement;
     let map;
-
     onMount(async () => {
-
+        await tick();
+        await tick();
         const leaflet = await import('leaflet');
-        48.760002485861406, -122.49076485842225
-        map = leaflet.map(mapElement).setView([48.76000, -122.490773], 18);
+        console.log('about to onmount map center', mapCenter);
+        map = leaflet.map(mapElement).setView(mapCenter, mapZoom);
 
         leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
--- mapperUI/src/routes/editor/+page.svelte
+++ mapperUI/src/routes/editor/+page.svelte
@@ -1,3 +1,58 @@
+<script>
+  import { onMount } from 'svelte';
+  import LeafletMap from '$lib/LeafletMap.svelte';
+  
+  let parks = [];
+  let selectedPark;
+  let genera = [] ;
+  let selectedGenus;
+  let species = [];
+  let selectedSpecies;
+  let commonName=''; 
+  let wiki = '';
+  let props={mapCenter:[ 48.760000, -122.490773 ],mapZoom:18};
+  
+  onMount(async function () {
+    const parkResponse = await fetch("http://localhost:8000/api/parks");
+    const myParks = await parkResponse.json();
+  
+    parks = myParks;
+    if(parks.length){
+      selectedPark=parks[0].id;
+      props.mapCenter=[parks[0].lat,parks[0].lng];
+      props.mapZoom=parks[0].zoom;
+      //props = {mapCenter:[parks[0].lat,parks[0].lng],mapZoom:parks[0].zoom};
+      console.log("setting init map", props)
+    }
+  
+    const generaResponse = await fetch("http://localhost:8000/api/genera");
+    const myGenera = await generaResponse.json();
+    genera = myGenera;
+  });
+  
+  async function changePark(){
+    console.log('changed park');
+  };
+  async function changeGenus(){
+    const resp = await fetch("http://localhost:8000/api/species/"+selectedGenus);
+    const mySpecies = await resp.json()
+    species = mySpecies;
+    selectedSpecies='';
+    commonName = '';
+    wiki = '';
+  };
+  
+  function changeSpecies(){
+   for (let i=0; i<species.length; i++){
+    if (species[i].id === selectedSpecies){
+        commonName = species[i].commonName;
+        wiki = species[i].wiki;
+      }
+    }
+  };
+  </script>
+
+
 <h3>Hello, Welcome to Tree Mapper!</h3>
 <label for="parks">Parks:</label>
 
@@ -30,57 +85,4 @@
 <p>{commonName}</p>
 <p>{wiki}</p>
 
-<LeafletMap />
-
-<script>
-import { onMount } from 'svelte';
-import LeafletMap from '$lib/LeafletMap.svelte';
-
-let parks = [];
-let selectedPark;
-let parkCenter = [];
-let parkZoom =18;
-let genera = [] ;
-let selectedGenus;
-let species = [];
-let selectedSpecies;
-let commonName=''; 
-let wiki = '';
-
-onMount(async function () {
-  const parkResponse = await fetch("http://localhost:8000/api/parks");
-  const myParks = await parkResponse.json();
-
-  parks = myParks;
-  if(parks.length){
-    selectedPark=parks[0].id;
-    parkCenter = [parks[0].lat,parks[0].lng];
-    parkZoom = parks[0].zoom;
-  }
-
-  const generaResponse = await fetch("http://localhost:8000/api/genera");
-  const myGenera = await generaResponse.json();
-  genera = myGenera;
-});
-
-async function changePark(){
-  console.log('changed park');
-};
-async function changeGenus(){
-  const resp = await fetch("http://localhost:8000/api/species/"+selectedGenus);
-  const mySpecies = await resp.json()
-  species = mySpecies;
-  selectedSpecies='';
-  commonName = '';
-  wiki = '';
-};
-
-function changeSpecies(){
- for (let i=0; i<species.length; i++){
-  if (species[i].id === selectedSpecies){
-      commonName = species[i].commonName;
-      wiki = species[i].wiki;
-    }
-  }
-};
-</script>
+<LeafletMap {...props}/>
